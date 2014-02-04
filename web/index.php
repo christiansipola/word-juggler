aa857a7e6a880f31ba0c3713f62ba0250ee90e79
Christian Sipola <christian@kaustik.com>
2014-02-04 19:52:58 +0100

    add more error handling

--- web/index.php
+++ web/index.php
@@ -1,15 +1,19 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+#ini_set('log_errors','1');
+ini_set('error_log', __DIR__ . '/error.log');
 use Zippo\FileReader;
 
 require_once __DIR__.'/../vendor/autoload.php';
-
 
 $app = new Silex\Application();
 // definitions
 $app['debug'] = true;
 
-$app->get('/', function () use ($app) {
-    $fileReader = new FileReader();    
-    return "hej";
+$fileReader = new FileReader();
+$app->get('/', function () use ($app, $fileReader) {
+    $fileReader->echoListOfRandomWords(20);
+    return "\n end \n";
 });
 $app->run();
